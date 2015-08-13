c32896b53eb107d3288d12df43e5e7df309890cf
Shen Hong <shen.hong@autodesk.com>
2015-08-13 11:45:18 -0700

    more comment

--- www/scripts/marker.js
+++ www/scripts/marker.js
@@ -294,6 +294,8 @@ function Marker(container, customMarker) {
 	var successiveCount = 0;
 	var successiveChange = "";
 
+		// external handlers for the marker, used for customized behavior for 
+		// mousedown/mousemove/mouseup events, called after the internal hanlders are done
 	var externalEvtHandlers = {
 		"canvasclick": [],
 		"markerdown": [],
@@ -309,6 +311,7 @@ function Marker(container, customMarker) {
 		};
 	}
 
+		// internal handler to keep the marker updated with mousemove events
 	var onmarkerdrag= function(evt) {
 		var currentTimestamp = Date.now();
 		if (currentTimestamp - lastTimestamp > REFRESHINTERVAL) {
@@ -443,10 +446,6 @@ function Marker(container, customMarker) {
 	this.getExtHandler = function() {
 		return externalEvtHandlers;
 	};
-
-	this.setCursorOffset = function() {
-
-	}
 }
 
 Marker.prototype.hideMarker = function() {
@@ -511,6 +510,7 @@ Marker.prototype.removeEventListenerOnMarker = function(eventName, callFunc) {
 	}
 };
 
+
 /**
  *  This class manages the direction data of the marker
  *  @class
@@ -519,6 +519,7 @@ Marker.prototype.removeEventListenerOnMarker = function(eventName, callFunc) {
  */
 
 function DirectionMap(direction) {
+
 	var normLen = 300;
 	this.getNormLen = function() {return normLen;};
 	this.currentDirection = direction || [0, normLen];
@@ -534,9 +535,11 @@ function DirectionMap(direction) {
 		{name:'SOUTH',		vector:[0, -1 * normLen],						xDeg:270},
 		{name:'SOUTHEAST',	vector:[0.7071 * normLen, -0.7071 * normLen],	xDeg:315},
 	];
+
 }
 
 DirectionMap.prototype.angleBetween = function(d1, d2) {
+
 	var length1 = Math.sqrt(d1[0]*d1[0]+d1[1]*d1[1]);
 	var length2 = Math.sqrt(d2[0]*d2[0]+d2[1]*d2[1]);
 	if (length1 === 0 || length2 === 0)
@@ -552,12 +555,14 @@ DirectionMap.prototype.angleBetween = function(d1, d2) {
 };
 
 DirectionMap.prototype.normalizeDirection = function(d, normLen) {
+
 	var length = Math.sqrt(d[0]*d[0]+d[1]*d[1]);
 	normLen = normLen || this.getNormLen();
 	return [normLen*(d[0]/length), normLen*(d[1]/length)];
 };
 
 DirectionMap.prototype.generalizeDirection = function(d) {
+
 	var angleToxAxis = Math.abs(this.angleBetween(d, [1,0]));
 	var index = Math.floor(angleToxAxis/(Math.PI/8));
 
@@ -571,104 +576,16 @@ DirectionMap.prototype.generalizeDirection = function(d) {
 };
 
 DirectionMap.prototype.setRotateCenter = function(clientX, clientY) {
+
 	this.rotateCenter.x = clientX;
 	this.rotateCenter.y = clientY;
 };
 
 DirectionMap.prototype.calcMouseDirection = function(clientX, clientY) {
+
 	var	subVec = [clientX-this.rotateCenter.x, this.rotateCenter.y-clientY];
 	return this.normalizeDirection(subVec);
 };
-
-
-function getPaperViewBox(point) {
-	var viewboxes = metadata[viewModels[currentModel].id].viewboxes;
-	for (var i = viewboxes.length - 1; i >= 0; i--) {
-		var viewbox = viewboxes[i].box;
-		if (point.x >= viewbox[0] && point.x <= viewbox[2] &&
-			point.y >= viewbox[3] && point.y <= viewbox[1]) {
-			return viewboxes[i];
-		}
-	};
-
-	return null;
-}
-
-function worldToPaper(position) {
-	var paperPos = {x:position.x * metadata[viewModels[currentModel].id].scale + metadata[viewModels[currentModel].id].xoffset, y:position.y * metadata[viewModels[currentModel].id].scale + metadata[viewModels[currentModel].id].yoffset, z:metadata[viewModels[currentModel].id].paperZ};
-
-	var floor = Math.floor((position.z - metadata[viewModels[currentModel].id].floorBase) / metadata[viewModels[currentModel].id].floorHeight);
-	floor = floor < 0 ? 0 : floor;
-	floor = floor >= metadata[viewModels[currentModel].id].viewboxes.length ? metadata[viewModels[currentModel].id].viewboxes.length - 1 : floor;
-
-	var viewbox = metadata[viewModels[currentModel].id].viewboxes[floor];
-	var xoffsetBox = viewbox.box[0] - metadata[viewModels[currentModel].id].viewboxes[0].box[0];
-    var yoffsetBox = viewbox.box[1] - metadata[viewModels[currentModel].id].viewboxes[0].box[1];
-
-    paperPos.x += xoffsetBox;
-    paperPos.y += yoffsetBox;
-
-	if (paperPos.x < viewbox.box[0] || paperPos.x > viewbox.box[2] ||
-		paperPos.y > viewbox.box[1] || paperPos.y < viewbox.box[3]) {
-		return null;
-	}
-	else {
-		return {pos:paperPos, boxIndex:floor};
-	}
-}
-
-function clientToWorld(normedPoint) {
-	var paperPos = clientToPaper(normedPoint);
-	var viewbox = getPaperViewBox(paperPos);
-    if (viewbox == null)
-    	return null;
-
-	var xoffsetBox = viewbox.box[0] - metadata[viewModels[currentModel].id].viewboxes[0].box[0];
-    var yoffsetBox = viewbox.box[1] - metadata[viewModels[currentModel].id].viewboxes[0].box[1];
-
-    return {x:(paperPos.x - metadata[viewModels[currentModel].id].xoffset - xoffsetBox) / metadata[viewModels[currentModel].id].scale, y:(paperPos.y - metadata[viewModels[currentModel].id].yoffset - yoffsetBox) / metadata[viewModels[currentModel].id].scale, z:(viewbox.floor * metadata[viewModels[currentModel].id].floorHeight) + (metadata[viewModels[currentModel].id].floorHeight/2 + metadata[viewModels[currentModel].id].floorBase)};
-}
-
-function clientToPaper(normedPoint) {
-    var hitPoint = viewer2D.utilities.getHitPoint(normedPoint.x, normedPoint.y);
-    if (hitPoint == null) {
-    	hitPoint = viewer2D.navigation.getWorldPoint(normedPoint.x, normedPoint.y);
-    }
-
-    return hitPoint;
-}
-
-function projectToViewport(position, camera) {
-    var p = new THREE.Vector4();
-
-    p.x = position.x;
-    p.y = position.y;
-    p.z = position.z;
-    p.w = 1;
-
-    p.applyMatrix4(camera.matrixWorldInverse);
-    p.applyMatrix4(camera.projectionMatrix);
-
-    if (p.w > 0)
-    {
-        p.x /= p.w;
-        p.y /= p.w;
-        p.z /= p.w;
-    }
-
-    var point = viewer2D.impl.viewportToClient(p.x, p.y);
-    point.x = Math.floor(point.x) + 0.5;
-    point.y = Math.floor(point.y) + 0.5;
-
-    return point;
-}
-
-function positionToVector3(position) {
-	var x = parseFloat(position[0] || position.x);
-	var y = parseFloat(position[1] || position.y);
-	var z = parseFloat(position[2] || position.z);
-    return new THREE.Vector3(x, y, z);
-}
 
 
 
@@ -725,4 +642,112 @@ function initDraggableDiv() {
 
         return false;
     };
+}
+
+
+
+
+
+
+	// NOTE: functions below handles the transformation between coordinate systems, for now the 
+	// relative location data (scale and offset) is hardcoded in rac.json, which is parsed into metadata.
+	// Also, as floor plans are only shown in part of the paper sheet, we define a viewbox for each floor,
+	// this is also hardwired, others include floor height and the base line.
+
+
+	// find the viewbox where the given point is located on a paper sheet
+function getPaperViewBox(point) {
+	var viewboxes = metadata[viewModels[currentModel].id].viewboxes;
+	for (var i = viewboxes.length - 1; i >= 0; i--) {
+		var viewbox = viewboxes[i].box;
+		if (point.x >= viewbox[0] && point.x <= viewbox[2] &&
+			point.y >= viewbox[3] && point.y <= viewbox[1]) {
+			return viewboxes[i];
+		}
+	};
+
+	return null;
+}
+
+	// transform 3D world position to paper position, i.e. position in viewer2D
+function worldToPaper(position) {
+	var paperPos = {x:position.x * metadata[viewModels[currentModel].id].scale + metadata[viewModels[currentModel].id].xoffset, y:position.y * metadata[viewModels[currentModel].id].scale + metadata[viewModels[currentModel].id].yoffset, z:metadata[viewModels[currentModel].id].paperZ};
+
+	var floor = Math.floor((position.z - metadata[viewModels[currentModel].id].floorBase) / metadata[viewModels[currentModel].id].floorHeight);
+	floor = floor < 0 ? 0 : floor;
+	floor = floor >= metadata[viewModels[currentModel].id].viewboxes.length ? metadata[viewModels[currentModel].id].viewboxes.length - 1 : floor;
+
+	var viewbox = metadata[viewModels[currentModel].id].viewboxes[floor];
+	var xoffsetBox = viewbox.box[0] - metadata[viewModels[currentModel].id].viewboxes[0].box[0];
+    var yoffsetBox = viewbox.box[1] - metadata[viewModels[currentModel].id].viewboxes[0].box[1];
+
+    paperPos.x += xoffsetBox;
+    paperPos.y += yoffsetBox;
+
+	if (paperPos.x < viewbox.box[0] || paperPos.x > viewbox.box[2] ||
+		paperPos.y > viewbox.box[1] || paperPos.y < viewbox.box[3]) {
+		return null;
+	}
+	else {
+		return {pos:paperPos, boxIndex:floor};
+	}
+}
+
+	// transform client point to 3D world position, i.e. position in viewer3D
+function clientToWorld(normedPoint) {
+	var paperPos = clientToPaper(normedPoint);
+	var viewbox = getPaperViewBox(paperPos);
+    if (viewbox == null)
+    	return null;
+
+	var xoffsetBox = viewbox.box[0] - metadata[viewModels[currentModel].id].viewboxes[0].box[0];
+    var yoffsetBox = viewbox.box[1] - metadata[viewModels[currentModel].id].viewboxes[0].box[1];
+
+    return {x:(paperPos.x - metadata[viewModels[currentModel].id].xoffset - xoffsetBox) / metadata[viewModels[currentModel].id].scale, y:(paperPos.y - metadata[viewModels[currentModel].id].yoffset - yoffsetBox) / metadata[viewModels[currentModel].id].scale, z:(viewbox.floor * metadata[viewModels[currentModel].id].floorHeight) + (metadata[viewModels[currentModel].id].floorHeight/2 + metadata[viewModels[currentModel].id].floorBase)};
+}
+
+	// transform client point to paper position, i.e. position in viewer2D
+function clientToPaper(normedPoint) {
+
+		// using hitpoint instead of scale/offset calculation
+    var hitPoint = viewer2D.utilities.getHitPoint(normedPoint.x, normedPoint.y);
+    if (hitPoint == null) {
+    	hitPoint = viewer2D.navigation.getWorldPoint(normedPoint.x, normedPoint.y);
+    }
+
+    return hitPoint;
+}
+
+	// project world/paper position to client canvas
+function projectToViewport(position, camera) {
+    var p = new THREE.Vector4();
+
+    p.x = position.x;
+    p.y = position.y;
+    p.z = position.z;
+    p.w = 1;
+
+    p.applyMatrix4(camera.matrixWorldInverse);
+    p.applyMatrix4(camera.projectionMatrix);
+
+    if (p.w > 0)
+    {
+        p.x /= p.w;
+        p.y /= p.w;
+        p.z /= p.w;
+    }
+
+    var point = viewer2D.impl.viewportToClient(p.x, p.y);
+    point.x = Math.floor(point.x) + 0.5;
+    point.y = Math.floor(point.y) + 0.5;
+
+    return point;
+}
+
+	// array|object to THREE.Vector3
+function positionToVector3(position) {
+	var x = parseFloat(position[0] || position.x);
+	var y = parseFloat(position[1] || position.y);
+	var z = parseFloat(position[2] || position.z);
+    return new THREE.Vector3(x, y, z);
 }
